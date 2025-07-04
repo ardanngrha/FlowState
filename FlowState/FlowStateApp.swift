1eebb312c59eb1ae535d481fca13f825594f7165
Ardana Nugraha <contact.ardana@gmail.com>
2025-07-04 08:54:41 +0700

    Initial Commit

---
+++ FlowState/FlowStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStateApp.swift
+//  FlowState
+//
+//  Created by Ardana Nugraha on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowStateTests/FlowStateTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlowStateTests.swift
+//  FlowStateTests
+//
+//  Created by Ardana Nugraha on 04/07/25.
+//
+
+import Testing
+
+struct FlowStateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
